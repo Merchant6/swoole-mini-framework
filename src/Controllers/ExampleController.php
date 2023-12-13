f054fc2e523b5d07b590cd19eb24865c89a1df0b
merchant <saboormerchant6@gmail.com>
2023-12-13 21:32:49 +0500

    Refactored controller code

--- src/Controllers/ExampleController.php
+++ src/Controllers/ExampleController.php
@@ -22,56 +22,16 @@ class ExampleController extends BaseController
 
     public function index(string $name, RequestContext $r)
     {
-        $msg = "Hello from $name";
-
-        return JsonResponse::make([
-            'data' => $msg,
-        ], 200);
+        
     }
 
     public function get(Entity $e, RequestContext $r)
     {   
-        $result = CoroutineManager::run(1, function () use($e, $r){
-            $query = $e->builder()
-            ->select('s')    
-            ->addSelect(['s.id', 's.fname', 's.lname'])
-            ->from(Swoole::class, 's');
-
-            $paginator = (new Paginator())
-            ->paginate($query, $r->getInstance()->get['page'] ?? 1, 20);
-
-            $result = [];
-            foreach($paginator->getItems() as $p)
-            {
-                $result[] = [
-                    'id'    => $p['id'],
-                    'fname' => $p['fname'],
-                    'lname' => $p['lname'],
-                ];
-            }
-
-            return $result;
-        });
-
-        return JsonResponse::make(['data' => $result], 200);
+        
     }
 
     public function form(Validator $validator, RequestContext $request)
     {
-        $validator->make($request, [
-            'text'=> 'length:2:20|string',
-            'email' => 'email'
-        ]);
-
-        if($validator->isValid())
-        {
-            return JsonResponse::make([
-                'data' => 'Text Validated',
-            ], 200);
-        }
-
-        return JsonResponse::make([
-            'message' => $validator->getErrors(),
-        ], 400);
+        
     }
 }
--- src/Entity/Entity.php
+++ src/Entity/Entity.php
@@ -5,25 +5,43 @@ use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\Tools\Setup;
+use Swoole\Coroutine as Co;
+use Swoole\Database\PDOConfig;
+use Swoole\Database\PDOPool;
+
 
 class Entity
 {
     /**
      * @var EntityManager
      */
+
+    /**
+     * @var PDOPool
+     */
+    protected PDOPool $pdoPool;
+
     protected EntityManager $entityManager;
 
     public function __construct()
     {
-        $config = require(__DIR__ . '/../Config/database.php');
+        $credentials = require(__DIR__ . '/../Config/database.php');
 
         $pool = new \Swoole\ConnectionPool(
-            function() use($config){
-                return DriverManager::getConnection($config['db']);
-            }, 100000
+            function() use ($credentials) {
+                return DriverManager::getConnection($credentials['db']);
+            },
+            100
         );
-
-        $this->entityManager =  EntityManager::create($pool->get(), Setup::createAttributeMetadataConfiguration([__DIR__ . '/../Entity']));
+        
+        $configuration = Setup::createAttributeMetadataConfiguration([__DIR__ . '/../Entity']);
+        
+        Co::create(function() use ($pool, $configuration) {
+            $connection = $pool->get();
+            $this->entityManager = EntityManager::create($connection, $configuration);
+            var_dump($pool->get());
+            $pool->close($connection);
+        });
     }
 
 
@@ -50,5 +68,24 @@ class Entity
         $entityManager = (new static)->entityManager;
         $entityManager->persist($entity);
         $entityManager->flush();
+    }
+
+    public function pool()
+    {
+        Co::create(function()
+        {
+            $pool = new PDOPool((new PDOConfig)
+            ->withHost('127.0.0.1')
+            ->withPort(3306)
+            ->withDbName('swoole-mini')
+            ->withCharset('utf8mb4')
+            ->withUsername('merchant')
+            ->withPassword('Thealien862!')
+            );
+
+            for ($n = 1024; $n--;) {
+                var_dump(json_encode($pool->get(), JSON_PRETTY_PRINT));
+            }
+        });
     }
 }
